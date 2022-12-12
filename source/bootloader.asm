5f92ddd570a0979749ad647f00f3bd8170b39d0c
Oussama Jaaouani <111194632+ntechnology0@users.noreply.github.com>
2022-12-13 00:25:58 +0100

    @update: printing brand

--- source/bootloader.asm
+++ source/bootloader.asm
@@ -1,3 +1,30 @@
-    times 510-($-$$) db 0 ; moves by 0s till the 510th byte.
+org 0x7c00            ; load code to memory
 
-    dw 0xaa55             ; boot BIOS magic number
+;; setting the video mode
+mov ah, 0x00          ; int 0x10 / ah 0x00 is for video mode
+mov al, 0x01          ; 40x25 text mode
+int 0x10              ; BIOS video interrupt
+
+mov ah, 0x0e          ; BIOS teleteype output int 10 / 0x0e ah
+mov bx, bootloader_name ; move memory address of string into BX register
+call print            ; calling our print function
+jmp stop
+
+print:
+    mov al, [bx]      ; mov character from bx to al
+    cmp al, 0
+    je end            ; jump if equal (al = 0) to halt label
+    int 0x10          ; BIOS video interrupt
+    add bx, 1         ; move to the next byte / get next character
+    jmp print
+
+end:
+    ret
+
+stop:
+    jmp $             ; ending program by looping jump here
+
+bootloader_name: db 'NOKTA Technologies', 0xA, 0xD, 0 ; 0/null to null terminate
+
+times 510-($-$$) db 0 ; moves by 0s till the 510th byte.
+dw 0xaa55             ; boot BIOS magic number / boot signature
